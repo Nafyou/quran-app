6da44fa2cb080eea29fdda34e616bdd5288b8430
julefikar <80801138+julefikar@users.noreply.github.com>
2022-06-12 12:56:57 -0400

    Added daily verse with basic styling

--- quran-react-frontend/src/App.test.js
+++ quran-react-frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Daily_Verse from './Daily_Verse';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Daily_Verse />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ quran-react-frontend/src/Daily_Verse.js
@@ -0,0 +1,13 @@
+import './Daily_Verse.css';
+
+function Daily_Verse() {
+  return (
+    <div className="Daily_Verse">
+        <div className="main_container">
+        <h1>Welcome to your Daily Verse</h1>
+        </div>
+    </div>
+  );
+}
+
+export default Daily_Verse
